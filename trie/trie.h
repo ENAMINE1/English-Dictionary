31fa56ee5dac1ed1142215043de52f781e3f7e1f
ENAMINE1 <kshashwat@kgpian.iitkgp.ac.in>
2023-02-10 22:17:11 +0530

    Header file containing two data types definition trienode and trie

--- trie/trie.h
+++ trie/trie.h
@@ -1,3 +1,20 @@
+#include<stdlib.h>
+
+// Structure of trie_node
+typedef struct node
+{
+    char alphabet;
+    struct node *child;
+    struct node *sibling;
+    int is_word;
+} trienode;
+
+// Structure of trie
+typedef struct Trie
+{
+    trienode *root;
+} trie;
+
 // Function to create a new node
 struct node *newNode(char);
 // Function to trieinsert a word in trie
