7cc7272426e223bec353dd478b855524a105a023
ENAMINE1 <kshashwat@kgpian.iitkgp.ac.in>
2023-02-10 11:07:54 +0530

    C file containing different operations on a dictionary

--- trie/trie.c
+++ trie/trie.c
@@ -22,12 +22,11 @@ struct node *newNode(char alphabet)
     return temp;
 }
 
-// Function to insert a word in trie
-void insert(struct node *root, char *word)
+// Function to trieinsert a word in trie
+void trieinsert(struct node *root, char *word)
 {
     // If trie is empty
     struct node *temp = root;
-    // If root is NULL, create a node and make it root
     int i = 0;
     // Traverse the trie with the given word
     while (word[i] != '\0')
@@ -44,23 +43,33 @@ void insert(struct node *root, char *word)
         // If current character is present as child
         else
         {
-            // Traverse the child nodes
-            temp = temp->child;
-            // If current character is not present as sibling
-            while (temp->sibling != NULL)
+            if (word[i] < temp->child->alphabet)
             {
-                // If current character is present as sibling
-                if (temp->alphabet == word[i])
-                    break;
-                // Else traverse the sibling nodes
-                temp = temp->sibling;
+                struct node *temp1 = newNode(word[i]);
+                temp1->sibling = temp->child;
+                temp->child = temp1;
+                temp = temp->child;
             }
-            // If current character is not present as sibling
-            if (temp->alphabet != word[i])
+            else
             {
-                // Create a new node and make it sibling of last child
-                temp->sibling = newNode(word[i]);
-                temp = temp->sibling;
+                // Traverse the child nodes
+                temp = temp->child;
+                // If current character is not present as sibling
+                while (temp->sibling != NULL)
+                {
+                    // If current character is present as sibling
+                    if (temp->alphabet == word[i])
+                        break;
+                    // Else traverse the sibling nodes
+                    temp = temp->sibling;
+                }
+                // If current character is not present as sibling
+                if (temp->alphabet != word[i])
+                {
+                    // Create a new node and make it sibling of last child
+                    temp->sibling = newNode(word[i]);
+                    temp = temp->sibling;
+                }
             }
         }
         i++;
@@ -68,8 +77,8 @@ void insert(struct node *root, char *word)
     temp->is_word = 1;
 }
 
-// Function to search a word in trie
-int search(struct node *root, char *word)
+// Function to triesearch a word in trie
+int triesearch(struct node *root, char *word)
 {
     // If trie is empty
     if (root == NULL)
@@ -106,54 +115,47 @@ int search(struct node *root, char *word)
 }
 
 // print the trie
-void print(struct node *root)
+void listall(struct node *root)
 {
     if (root == NULL)
         return;
     struct node *temp = root;
     // while (temp != NULL)
-    static char a[100];
-    // {
-    //     printf("%c", temp->alphabet);
-    //     print(temp->child);
-    //     print(temp->sibling);
-    // }
+    static char a[100]
     a[strlen(a)] = temp->alphabet;
-    // printf("%d \n", strlen(a));
     if (temp->is_word == 1)
     {
         printf("%s\n", a);
     }
-    print(temp->child);
+    listall(temp->child);
     a[strlen(a) - 1] = '\0';
-    // printf("%s\n", a);
-
-    print(temp->sibling);
+    listall(temp->sibling);
 }
 
 // Driver program to test above functions
-int main()
-{
-    struct node *root = NULL;
-    root = newNode('\0');
-    insert(root, "hello");
-    insert(root, "world");
-    insert(root, "hell");
-    insert(root, "word");
-    insert(root, "hi");
-    insert(root, "bye");
-    print(root);
-    printf("%d ", search(root, "hello"));
-    printf("%d ", search(root, "world"));
-    printf("%d ", search(root, "hell"));
-    printf("%d ", search(root, "word"));
-    printf("%d ", search(root, "hi"));
-    printf("%d ", search(root, "bye"));
-    printf("%d ", search(root, "he"));
-    printf("%d ", search(root, "wor"));
-    printf("%d ", search(root, "hel"));
-    printf("%d ", search(root, "worl"));
-    printf("%d ", search(root, "h"));
-    printf("%d ", search(root, "b"));
-    return 0;
-}
+// int main()
+// {
+//     struct node *root = NULL;
+//     root = newNode('\0');
+//     trieinsert(root, "hello");
+//     trieinsert(root, "world");
+//     trieinsert(root, "hell");
+//     trieinsert(root, "word");
+//     trieinsert(root, "hi");
+//     trieinsert(root, "he");
+//     trieinsert(root, "bye");
+//     listall(root);
+//     printf("%d ", triesearch(root, "hello"));
+//     printf("%d ", triesearch(root, "world"));
+//     printf("%d ", triesearch(root, "hell"));
+//     printf("%d ", triesearch(root, "word"));
+//     printf("%d ", triesearch(root, "hi"));
+//     printf("%d ", triesearch(root, "bye"));
+//     printf("%d ", triesearch(root, "he"));
+//     printf("%d ", triesearch(root, "wor"));
+//     printf("%d ", triesearch(root, "hel"));
+//     printf("%d ", triesearch(root, "worl"));
+//     printf("%d ", triesearch(root, "h"));
+//     printf("%d ", triesearch(root, "b"));
+//     return 0;
+// }
