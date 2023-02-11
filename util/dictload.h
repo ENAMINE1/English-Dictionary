15a39442b1fc82de2c6da2e508070d4e5ff2d7b2
ENAMINE1 <kshashwat@kgpian.iitkgp.ac.in>
2023-02-11 10:34:15 +0530

    Header file for containing definitions of functions to load a txt file

---
+++ util/dictload.h
@@ -0,0 +1,5 @@
+#include"dicttype.h"
+//Function to load default dictionary
+void loadaddfltdict(trienode *root);
+// Function to load a dictionary into a trie
+void loaddict(char *filename, trienode *root);
