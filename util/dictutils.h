90f5b589172c8541d8f49656ac2d62934891f04d
ENAMINE1 <kshashwat@kgpian.iitkgp.ac.in>
2023-02-12 09:11:13 +0530

    Header file for dictionary utilities

--- util/dictutils.h
+++ util/dictutils.h
@@ -1,1 +1,7 @@
 #include "dictload.h"
+
+// The function should return a single string consisting of all single letters that can be added before S to obtain valid English words.
+char *addbefore(dict D, char *S);
+
+// The function should return a single string consisting of all single letters that can be added after S to obtain valid English words.
+char * addafter(dict D, char *S);
