0bbeb096916ef8b44c67e69c2833f2244d0f9e48
ENAMINE1 <kshashwat@kgpian.iitkgp.ac.in>
2023-02-14 20:21:22 +0530

    Function that gives all the valid permutations of all substring of length one less than the original string

---
+++ apps/subwords.c
@@ -0,0 +1,42 @@
+#include "../util/dictutils.c"
+
+int main()
+{
+    char _str[] = "potsp";
+    // scanf("%s", _str);
+    // char *str = strdup(_str);
+    dict D;
+    D.root = NULL;
+    D.root = newNode('\0');
+    D.root = loadaddfltdict(D.root);
+    int i, len = strlen(_str);
+    for (i = 0; i < len; ++i)
+    {
+
+        char *temp_str;
+        temp_str = (char *)malloc((len) * sizeof(char));
+        int j, k = 0;
+        for (j = 0; j < len; ++j)
+        {
+            if (j == i)
+            {
+            }
+            else
+            {
+                temp_str[k] = _str[j];
+                k++;
+            }
+        }
+        temp_str[k] = '\0';
+        char *temp = strdup(temp_str);
+        char **list = sortedPermutations(D, temp);
+        int l = 0;
+        while (list[l] != NULL)
+        {
+            printf("%s\n", list[l]);
+            l++;
+        }
+        free(list);
+        free(temp_str);
+    }
+}
