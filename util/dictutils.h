67a6159a352ad2ae04cafc27c7d13f154299e037
ENAMINE1 <kshashwat@kgpian.iitkgp.ac.in>
2023-02-24 06:30:16 +0530

    Dichutil.h

--- util/dictutils.h
+++ util/dictutils.h
@@ -5,3 +5,6 @@ char *addbefore(dict D, char *S);
 
 // The function should return a single string consisting of all single letters that can be added after S to obtain valid English words.
 char * addafter(dict D, char *S);
+
+// This function gives you sorted permutation of a string
+char **sortedPermutations(dict D, char s[]);
